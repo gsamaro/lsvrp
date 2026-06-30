257612d630ea2b1b1140bff3afe64e3f0a653e43
gabriel <gabriel.amaro94@gmail.com>
2026-06-30 19:01:23 -0300

    removing files

--- production-routing-problem/tests/ProductionRoutingProblemPRP1Tests.py
+++
@@ -1,8 +0,0 @@
-import json
-
-from engines.dtos.Data import Data
-
-
-def writeDto(data=Data, c="", i=""):
-    with open("./out/dtoValidate/" + c + "_clients_" + i + ".json", "w") as arquivo:
-        arquivo.write(json.dumps(data.__dict__))
